75c82b1e2e2aacadf6918ea6b95b2c9cc8497bf5
Lucas Araujo <lucas.araujo@indicium.tech>
2025-09-03 22:58:37 +0000

    add first macro and apply to model

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,25 +1,9 @@
-with 
-
-source as (
-
-    select * from {{ source('stripe', 'payment') }}
-
-),
-
-transformed as (
-
-  select
-
+select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    created as payment_created_at,
-    amount as amount,
-    status as payment_status,
-    round(amount / 100.0, 2) as payment_amount
-
-  from source
-
-)
-
-select * from transformed
+    status,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
+    created as created_at
+from {{ source('stripe','payment') }}
